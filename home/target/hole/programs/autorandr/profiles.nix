c82fa3223435b6f601336ecad675e34ad085263a
Sukant Hajra <rrudbskr5g@snkmail.com>
2022-04-02 16:47:00 -0500

    new monitor at home

--- home/target/hole/programs/autorandr/profiles.nix
+++ home/target/hole/programs/autorandr/profiles.nix
@@ -1,40 +1,36 @@
 i3-dpi:
 
 let
-    DP-1-8 = import ./fingerprint.DP-1-8.nix;
+    DP-1 = import ./fingerprint.DP-1.nix;
     eDP-1  = import ./fingerprint.eDP-1.nix;
 in
 
 {
     home = {
-        fingerprint = { inherit DP-1-8 eDP-1; };
+        fingerprint = { inherit DP-1 eDP-1; };
         config = {
-            DP-1-1.enable = false;
-            DP-1.enable = false;
+            eDP-1.enable = false;
             HDMI-1.enable = false;
             HDMI-2.enable = false;
-            eDP-1.enable = false;
-            DP-1-8 = {
+            DP-1 = {
                 enable = true;
-                dpi = 150;
+                dpi = 140;
                 primary = true;
                 position = "0x0";
-                mode = "2560x1440";
-                rate = "59.95";
+                mode = "3840x2160";
+                rate = "60.00";
             };
         };
         hooks.preswitch = ''
-            ${i3-dpi}/bin/i3-dpi 150
+            ${i3-dpi}/bin/i3-dpi 140
         '';
     };
     laptop = {
         fingerprint = { inherit eDP-1; };
         config = {
-            DP-1-1.enable = false;
             DP-1.enable = false;
             HDMI-1.enable = false;
             HDMI-2.enable = false;
-            DP-1-8.enable = false;
             eDP-1 = {
                 enable = true;
                 dpi = 235;
